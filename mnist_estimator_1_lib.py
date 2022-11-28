5e336c7bf3a8bed017d6f0512f4c5230fd7a19c2
Matthew Krafczyk <mkrafcz2@hli-login01.holli.ncsa.illinois.edu>
2022-11-28 10:36:10 -0600

    Use data_format channels first.

--- mnist_estimator_1_lib.py
+++ mnist_estimator_1_lib.py
@@ -24,6 +24,15 @@ def input_fn(mode, params={}, input_context=None):
         num_parallel_calls=tf.data.experimental.AUTOTUNE
     )
 
+    # Data needs to be in channels first format.
+    def transpose_img(image, label):
+        return tf.transpose(image, perm=[2, 0, 1]), label
+
+    mnist_dataset = mnist_dataset.map(
+        transpose_img,
+        num_parallel_calls=tf.data.experimental.AUTOTUNE
+    )
+
     if input_context:
         mnist_dataset = mnist_dataset.shard(
             input_context.num_input_pipelines,
@@ -55,6 +64,10 @@ def normalize_img_32(image, label):
     return tf.cast(image, tf.float32) / 255., tf.cast(label, tf.int32)
 
 
+def img_channels_first(image, label):
+    return tf.transpose(image, perm=[2, 0, 1]), label
+
+
 def flatten(images, label):
     batch_size = images.shape[0]
     images = tf.reshape(images, [batch_size, -1])
@@ -62,36 +75,41 @@ def flatten(images, label):
 
 
 def train_input_fn(params={}, input_context=None):
-        # setting num_parallel_calls to 0 implies AUTOTUNE
-        num_parallel_calls = params.get("num_parallel_calls", 0)
-        if num_parallel_calls == 0:
-            num_parallel_calls = tf.data.experimental.AUTOTUNE
+    # setting num_parallel_calls to 0 implies AUTOTUNE
+    num_parallel_calls = params.get("num_parallel_calls", 0)
+    if num_parallel_calls == 0:
+        num_parallel_calls = tf.data.experimental.AUTOTUNE
 
-        datasets, info = tfds.load(
-            name='mnist',
-            with_info=True,
-            as_supervised=True)
-        mnist_dataset = datasets['train']
-        num_examples = info.splits['train'].num_examples
+    datasets, info = tfds.load(
+        name='mnist',
+        with_info=True,
+        as_supervised=True)
+    mnist_dataset = datasets['train']
+    num_examples = info.splits['train'].num_examples
 
-        mnist_dataset = mnist_dataset.map(
-            normalize_img_16,
-            num_parallel_calls=num_parallel_calls
-        )
+    mnist_dataset = mnist_dataset.map(
+        normalize_img_16,
+        num_parallel_calls=num_parallel_calls
+    )
 
-        if input_context:
-            mnist_dataset = mnist_dataset.shard(
-                input_context.num_input_pipelines,
-                input_context.input_pipeline_id)
+    mnist_dataset = mnist_dataset.map(
+        img_channels_first,
+        num_parallel_calls=num_parallel_calls
+    )
 
-        batch_size = 32
-        mnist_dataset = mnist_dataset.cache() \
-                                     .shuffle(num_examples) \
-                                     .batch(batch_size, drop_remainder=True) \
-                                     .prefetch(tf.data.experimental.AUTOTUNE) \
-                                     .repeat()
+    if input_context:
+        mnist_dataset = mnist_dataset.shard(
+            input_context.num_input_pipelines,
+            input_context.input_pipeline_id)
 
-        return mnist_dataset
+    batch_size = 32
+    mnist_dataset = mnist_dataset.cache() \
+                                 .shuffle(num_examples) \
+                                 .batch(batch_size, drop_remainder=True) \
+                                 .prefetch(tf.data.experimental.AUTOTUNE) \
+                                 .repeat()
+
+    return mnist_dataset
 
 
 def test_input_fn(params={}, input_context=None):
@@ -109,6 +127,11 @@ def test_input_fn(params={}, input_context=None):
 
     mnist_dataset = mnist_dataset.map(
         normalize_img_16,
+        num_parallel_calls=num_parallel_calls
+    )
+
+    mnist_dataset = mnist_dataset.map(
+        img_channels_first,
         num_parallel_calls=num_parallel_calls
     )
 
@@ -158,7 +181,7 @@ def model_fn(features, labels, mode, params):
         #ReshapeLayer((28*28*1,), input_shape=(28,28,1)),
         #tf.keras.layers.Dense(128, activation='relu'),
         #tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Conv2D(32, 3, dilation_rate=(2,2), activation='relu', input_shape=(28,28,1)),
+        tf.keras.layers.Conv2D(32, 3, dilation_rate=(2,2), activation='relu', input_shape=(1,28,28), data_format='channels_first'),
         tf.keras.layers.Flatten(),
         #tf.keras.layers.Dense(10, activation='linear')
         tf.keras.layers.Dense(10, activation='linear')
